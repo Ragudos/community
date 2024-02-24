2834032cdbf9d3ad74972bac96bfdcf07323e56e
Aaron <133567781+Ragudos@users.noreply.github.com>
2024-02-24 20:13:19 +0800

    Update and rename migrations.sh to ci_migrations.sh

--- scripts/__test-utils__/ci_migrations.sh
+++ scripts/__test-utils__/ci_migrations.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
 
-## Set PostgreSQL connection details
-PG_HOST = "localhost"
-PG_PORT = "5432"
-PG_USER="your_username"
-PG_PASSWORD="your_password"
-PG_DATABASE="your_database"
-
-# Set the path to your migration folder
-MIGRATION_FOLDER="/db/sqlx/migrations"
-
 # Iterate through each file in the migration folder
-for FILE in "$MIGRATION_FOLDER"/*
+for FILE in "/db/sqlx/migrations"/*
 do
     if [ -f "$FILE" ]; then
         echo "Running migration: $FILE"
-        psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $PG_DATABASE -f "$FILE"
+        psql -h $PG_HOST -p $PG_PORT -U $PG_USER -d $PG_DATABAS -f "$FILE"
         if [ $? -eq 0 ]; then
             echo "Migration successful"
         else
