f55f1d95abaa1de2612f291439577d3bb8b8ca69
Aaron Ragudos <workemailaaronragudos@gmail.com>
2024-03-23 02:05:21 +0800

    Changed dropdown to be details & logout to use Redirect

--- client/src/dropdown.ts
+++ client/src/dropdown.ts
@@ -1,80 +1,35 @@
 export function initDropdown() {
     document.addEventListener("click", onDocumentClick);
-    activateDropdowns();
 }
 
 function onDocumentClick(evt: MouseEvent) {
-    const openDropdown = document.querySelector(".dropdown[data-active=open]"),
-        documentTarget = evt.target;
+    const openDropdown = document.querySelector("details.dropdown[open]");
 
-    if (!(documentTarget instanceof HTMLElement)) {
+    if (!(openDropdown instanceof HTMLDetailsElement)) {
         return;
     }
 
-    const button = openDropdown.querySelector("button[data-opener]") as HTMLButtonElement | null;
+    const summary = openDropdown.querySelector("summary");
 
-    if (!button) {
-        return console.warn("No button found in dropdown");
-    }
+    if (evt.target === summary) {
+        return;
+    } else {
+        const summaryChildren = Array.from(summary?.querySelectorAll("*") ?? []);
 
-    const targetId = button.getAttribute("aria-controls"),
-        target = document.getElementById(targetId);
-
-    if (!target) {
-        return console.warn(`No target found for dropdown with id ${targetId}`);
-    }
-
-    openDropdown.removeAttribute("data-active");
-    button.setAttribute("aria-expanded", "false");
-    removeTabIndexFromButtons(getAllButtons(target));
-}
-
-function activateDropdowns() {
-    const dropdowns = document.querySelectorAll(".dropdown");
-
-    for (let i = 0; i < dropdowns.length; ++i) {
-        const dropdown = dropdowns[i],
-            button = dropdown.querySelector("button[data-opener]") as HTMLButtonElement | null;
-
-        if (!button) {
-            console.warn("No button found in dropdown ", dropdown);
-            continue;
-        }
-
-        button.addEventListener("click", onDropdownButtonClick(dropdown, button));
-    }
-}
-
-function onDropdownButtonClick(dropdown: Element, button: HTMLButtonElement) {
-    return (evt: MouseEvent) => {
-        const targetId = button.getAttribute("aria-controls"),
-            target = document.getElementById(targetId);
-
-        if (!target) {
-            return console.warn(`No target found for dropdown with id ${targetId}`);
-        }
-
-        if (button.getAttribute("aria-expanded") === "true") {
-            button.setAttribute("aria-expanded", "false");
-            dropdown.removeAttribute("data-active");
-            removeTabIndexFromButtons(getAllButtons(target));
-        } else {
-            button.setAttribute("aria-expanded", "true");
-            dropdown.setAttribute("data-active", "true");
-            addTabIndexToButtons(getAllButtons(target));
+        for (let i = 0; i < summaryChildren.length; ++i) {
+            if (evt.target === summaryChildren[i]) {
+                return;
+            }
         }
     }
+
+    openDropdown.removeAttribute("open");
+    removeTabIndexFromButtons(getAllButtons(openDropdown));
 }
 
 function removeTabIndexFromButtons(buttons: (HTMLButtonElement | HTMLAnchorElement)[]) {
     for (let idx = 0; idx < buttons.length; ++idx) {
         buttons[idx].removeAttribute("tabindex");
-    }
-}
-
-function addTabIndexToButtons(buttons: (HTMLButtonElement | HTMLAnchorElement)[]) {
-    for (let idx = 0; idx < buttons.length; ++idx) {
-        buttons[idx].setAttribute("tabindex", "0");
     }
 }
 
--- src/routes/auth/api/logout.rs
+++ src/routes/auth/api/logout.rs
@@ -1,13 +1,13 @@
 use rocket::delete;
 use rocket::http::CookieJar;
+use rocket::response::Redirect;
 
 use crate::auth_uri;
-use crate::controllers::htmx::redirect::HtmxRedirect;
 use crate::models::JWT_NAME;
 use crate::routes::auth::login;
 
 #[delete("/logout")]
-pub fn delete(cookie_jar: &CookieJar<'_>) -> HtmxRedirect {
+pub fn delete(cookie_jar: &CookieJar<'_>) -> Redirect {
     cookie_jar.remove_private(JWT_NAME);
-    HtmxRedirect::to(auth_uri!(login::page))
+    Redirect::to(auth_uri!(login::page))
 }
